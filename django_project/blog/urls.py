1ad1f3e2dc68d3a3e8ea7df93259f0ab764395c2
Clyde <ibalezclyde91@gmail.com>
2019-12-15 11:28:30 +0400

    Create, Update and Delete Posts

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views  # .(dot) means I imported it from the same directory
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
     path('about/', views.about, name="blog-about")
 ]
+
+# blog/post_list.html
+# <app>/<model>_<viewtype>.html
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -7,6 +9,52 @@ def home(request):
         "posts": Post.objects.all()  # Query from database that will get all the objects
     }
     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-Date_Posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['Title', 'Content']
+
+    def form_valid(self, form):
+        form.instance.Author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['Title', 'Content']
+
+    def form_valid(self, form):
+        form.instance.Author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.Author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.Author:
+            return True
+        return False
 
 
 def about(request):
